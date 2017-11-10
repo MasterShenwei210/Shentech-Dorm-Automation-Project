4a14b5a9d70ef4b0accbff78828601380a420017
MasterShenwei210 <sherwina210@gmail.com>
2017-11-10 15:58:43 -0800

    dorm ex machima

--- Clock_ESP8266/Clock_ESP8266.ino
+++ Clock_ESP8266/Clock_ESP8266.ino
@@ -28,6 +28,10 @@
 *                        (2 to pause song)
 *                        (3 to go to next song)
 *                        (4 to go to previous song)
+*                        (5 to wake up)
+*                        (6 to sleep)
+*                        (7 to increase volume)
+*                        (8 to decrease volume)
 *              automation= (0 to turn off mp3 player automation)
 *                          (1 to turn on mp3 player automation)
 *              onhour  = (hour to turn on mp3 player in military time 0-23)
@@ -43,6 +47,14 @@
 *              onmin   = (minute to display message 0-50)
 *              offhour = (hour to hide message in military time 0-23)
 *              offmin  = (minute to hide message 0-50)
+*              brightness = (set brightness of lcd, pass in value 0-255)
+*              message = (type out message. put '-' in each space and
+*                          set maximum of 40 characters)
+*
+* /bluetooth:
+*   function: turn bluetooth on or offmin
+*   arguments: state   = (0 to turn off bluetooth)
+*                      = (1 to turn on bluetooth)
 *
 * /lampcommand:
 *   function: send command to music lamps
@@ -55,6 +67,8 @@
 #include <ESP8266mDNS.h>
 #include <ESP8266HTTPClient.h>
 
+#define timer0_preload 40161290
+#define DELAY_TIME        6
 #define LAMP_STATE        0
 #define LAMP_DISPLAY      1
 #define LAMP_ON           1
@@ -81,12 +95,30 @@
 #define PAUSE             2
 #define NEXT_SONG         3
 #define PREVIOUS_SONG     4
+#define WAKE_UP           5
+#define SLEEP             6
+#define VOL_UP            7
+#define VOL_DOWN          8
+
 #define REMINDER_STATE    0
 #define REMINDER_ON_HOUR  1
 #define REMINDER_ON_MIN   2
 #define REMINDER_OFF_HOUR 3
 #define REMINDER_OFF_MIN  4
 #define REMINDER_MESSAGE  5
+
+#define YEAR              1
+#define MONTH             2
+#define DAY               3
+#define HOUR              4
+#define MIN               5
+
+#define IP_TYPE           5
+#define TIME_TYPE         1
+#define ALARM_TYPE        2
+#define MP3_TYPE          3
+#define REMINDER_TYPE     4
+#define BLUETOOTH_TYPE    6
 
 String payload;
 String time_out;
@@ -97,18 +129,27 @@ int data_recieved[5];
 int hour,min,sec;
 int day, month, year;
 int lamp_command;
-String reminder_checks[6] = {"state","onhour","onmin","offhour","offmin","message"};
-int reminder_data[5] = {0,0,0,0,0};
+String reminder_checks[7] = {"state","onhour","onmin","offhour","offmin","brightness","message"};
+int reminder_data[6] = {0,0,0,0,0,255};
 String reminder_message = " ";
 String mp3_checks[6] = {"command","automation","onhour","onmin","offhour","offmin"};
 int mp3_data[6] = {0,0,0,0,0,0};
 int alarm_data[3] = {0,0,0};
 String lamp_checks[6] = {"state","display","onhour","onmin","offhour","offmin"};
 int lamp_data[6] = {0,0,0,0,0,0};
+byte time_data[6];
 String alarm_checks[3] = {"state","hour","min"};
-
+String bluetooth_checks[1] = {"state"};
+int bluetooth_data[1] = {0};
+String ip_address;
+bool send_alarm = false,send_mp3 = false,send_remind = false;
 ESP8266WebServer server(80);
 HTTPClient http;
+unsigned long last_time = 0;
+unsigned long this_time;
+
+const char* ssid = "MasterShenwei";
+const char* password = "sherwin210";
 
 void start_up(const char* ssid, const char* password){
   WiFi.begin(ssid, password);
@@ -116,13 +157,14 @@ void start_up(const char* ssid, const char* password){
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    //Serial.print(".");
   }
-  /*Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());*/
+//  Serial.println("");
+//  Serial.print("Connected to ");
+  //Serial.println(ssid);
+
+  ip_address = String(WiFi.localIP());
+  Serial.write((byte)IP_TYPE);
+  Serial.print(WiFi.localIP());
 
   if (MDNS.begin("esp8266")) {
     //Serial.println("MDNS responder started");
@@ -169,10 +211,10 @@ void check_arguments(String raw_arguments[], String raw_data[], String check_str
   }
 }
 
-void send_data(String type, int data[],int end,int begin = 0){
-  Serial.print(type);
+void send_data(int type, int data[],int end,int begin = 0){
+  Serial.write((byte)type);
   for(int i = begin; i < end; i++){
-    Serial.write(data[i]);
+    Serial.write((byte)data[i]);
   }
 }
 
@@ -195,7 +237,7 @@ void update_alarm(){
   if(argument_count > 0){
     get_arguments(argument_count);
     check_arguments(name_buffer,buffer,alarm_checks,alarm_data,argument_count,3);
-    send_data("alarm",alarm_data,3);
+    send_data(ALARM_TYPE,alarm_data,3);
   }
   String message = "alarm time set ";
   if(alarm_data[ALARM_STATE]){
@@ -310,7 +352,7 @@ void adjust_mp3(){
   if (argument_count > 0){
     get_arguments(argument_count);
     check_arguments(name_buffer,buffer,mp3_checks,mp3_data,argument_count,6);
-    send_data("mp3",mp3_data,6);
+    send_data(MP3_TYPE,mp3_data,6);
   }
   String message = "mp3 told to ";
   switch (mp3_data[MP3_COMMAND]) {
@@ -319,6 +361,11 @@ void adjust_mp3(){
     case PAUSE: message += "pause song";break;
     case NEXT_SONG: message += "go to next song";break;
     case PREVIOUS_SONG: message += "go to previous song";break;
+    case WAKE_UP: message += "wake up";break;
+    case SLEEP: message += "go to sleep";break;
+    case VOL_UP: message += "increase volume";break;
+    case VOL_DOWN: message += "decrease volume";break;
+    default: message += "do nothing";break;
   }
   message +="\n\nautomation is ";
   if(mp3_data[MP3_AUTOMATION]){
@@ -336,18 +383,47 @@ void adjust_mp3(){
   mp3_data[MP3_COMMAND] = DO_NOTHING;
 }
 
+void set_bluetooth(){
+  int argument_count =  server.args();
+  if(argument_count > 0){
+    get_arguments(argument_count);
+    check_arguments(name_buffer,buffer,bluetooth_checks,bluetooth_data,argument_count,1);
+    Serial.write(6);
+    Serial.write((byte) bluetooth_data[0]);
+  }
+  String message = "bluetooth set ";
+  if(bluetooth_data[0]){
+    message += "on ";
+  }
+  else{
+    message += "off ";
+  }
+  server.send(200,"text/plain", message);
+}
+
 void set_reminder(){
   int argument_count = server.args();
   if(argument_count > 0){
     get_arguments(argument_count);
-    check_arguments(name_buffer,buffer,reminder_checks,reminder_data,argument_count,5);
+    check_arguments(name_buffer,buffer,reminder_checks,reminder_data,argument_count,6);
 
     for(int i = 0; i < argument_count; i++){
-      if(name_buffer[i] == reminder_checks[REMINDER_MESSAGE]){
+      if(name_buffer[i] == reminder_checks[REMINDER_MESSAGE+1]){
         reminder_message = buffer[i];
       }
     }
-    send_data("reminder",reminder_data,5);
+    int mes_length = reminder_message.length();
+
+    byte send_buffer[8];
+    send_buffer[0] = REMINDER_TYPE;
+    send_buffer[1] = mes_length;
+    send_buffer[2] = reminder_data[REMINDER_STATE];
+    send_buffer[3] = reminder_data[REMINDER_ON_HOUR];
+    send_buffer[4] = reminder_data[REMINDER_ON_MIN];
+    send_buffer[5] = reminder_data[REMINDER_OFF_HOUR];
+    send_buffer[6] = reminder_data[REMINDER_OFF_MIN];
+    send_buffer[7] = reminder_data[5];
+    Serial.write(send_buffer,8);
     Serial.print(reminder_message);
   }
   String message = "reminder is: ";
@@ -363,6 +439,8 @@ void set_reminder(){
   message += time_string(reminder_data[REMINDER_ON_HOUR],reminder_data[REMINDER_ON_MIN]);
   message += "\nhide at ";
   message += time_string(reminder_data[REMINDER_OFF_HOUR],reminder_data[REMINDER_OFF_MIN]);
+  message += "\nbrightness set to ";
+  message += (String) reminder_data[5];
 
   server.send(200,"text/plain",message);
 }
@@ -376,15 +454,15 @@ void decode_time_out(String data_in){
   buffer[4] = data_in.substring(15,17);
   buffer[5] = data_in.substring(18,20);
 
-  year = buffer[0].toInt();
-  month = buffer[1].toInt();
-  day = buffer[2].toInt();
-  hour = buffer[3].toInt();
-  min = buffer[4].toInt();
-  sec = buffer[5].toInt();
+  time_data[YEAR] = (byte) (buffer[0].toInt() - 2000);
+  time_data[MONTH] = (byte) buffer[1].toInt();
+  time_data[DAY] = (byte) buffer[2].toInt();
+  time_data[HOUR] =(byte) buffer[3].toInt();
+  time_data[MIN] = (byte) buffer[4].toInt();
 }
+
 void setup(){
-  Serial.begin(115200);
+  Serial.begin(9600);
   start_up("MasterShenwei","sherwin210");
 
   server.on("/", show_home_page);
@@ -393,34 +471,29 @@ void setup(){
   server.on("/lampcommand", send_lamp_command);
   server.on("/mp3", adjust_mp3);
   server.on("/reminder", set_reminder);
+  server.on("/bluetooth", set_bluetooth);
   server.onNotFound(handleNotFound);
 
   server.begin();
-  //Serial.println("HTTP server started");
-  http.begin("http://api.timezonedb.com/v2/get-time-zone?key=60K12JHZKLV3&by=zone&zone=America/Los_Angeles&format=json&fields=formatted");
-
-
 }
 
 void loop(){
-  http_code = http.GET();
-  //Serial.println(http_code);
-  if(http_code > 0){
-    //Serial.println(HTTP_CODE_OK);
-    if(http_code == HTTP_CODE_OK){
-      payload = http.getString();
-      time_out = payload.substring(40);
-      decode_time_out(time_out);
-      Serial.print("time/date");
-      Serial.write(year);
-      Serial.write(month);
-      Serial.write(day);
-      Serial.write(hour);
-      Serial.write(min);
-      Serial.write(sec);
+  this_time = millis();
+  if(this_time - last_time >= 5000){
+    http.begin("http://api.timezonedb.com/v2/get-time-zone?key=60K12JHZKLV3&by=zone&zone=America/Los_Angeles&format=json&fields=formatted");
+    http_code = http.GET();
+    if(http_code > 0){
+      if(http_code == HTTP_CODE_OK){
+          payload = http.getString();
+          time_out = payload.substring(40);
+          decode_time_out(time_out);
+          time_data[0] = (byte) TIME_TYPE;
+          Serial.write(time_data,6);
+
+      }
     }
-
+    http.end();
+    last_time = this_time;
   }
-
-    server.handleClient();
+  server.handleClient();
 }
